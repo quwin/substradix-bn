2b9e26cd987a1f0a7536bddf0cfcfca0dadcf832
Ethan Tran <ethantran.et77@gmail.com>
2022-04-27 09:19:24 -0700

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -24,9 +24,6 @@ impl Fr {
     pub fn one() -> Self {
         Fr(fields::Fr::one())
     }
-    pub fn random<R: Rng>(rng: &mut R) -> Self {
-        Fr(fields::Fr::random(rng))
-    }
     pub fn pow(&self, exp: Fr) -> Self {
         Fr(self.0.pow(exp.0))
     }
